af7b55608d85eb8db0b66649de94920472e6b8cd
Moshe Zadka <zadka.moshe@gmail.com>
2020-04-04 19:32:00 -0700

    New example

---
+++ examples/README.rst
@@ -0,0 +1,2 @@
+This example uses sprites from
+https://kenney.nl/assets/animal-pac
---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools 
+
+setuptools.setup(
+      name='txppb',
+      version='0.1',
+      packages=['txppb'],
+)
+
